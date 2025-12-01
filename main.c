4bb580488719cba83e5cd6e4e709f535f459e730
julie-wng <julieweng2011@gmail.com>
2025-12-01 16:47:22 +0100

    dossier

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//#include "projet.h"
+
+int main(){
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
---
+++ projet.h
@@ -0,0 +1,13 @@
+#ifndef PROJET_H_INCLUDED
+#define PPROJET_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "affichage_console.h"
+
+#define LIGNE 15
+#define COLONNE 10
+
+
+#endif
