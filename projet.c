9e68c9dcc77a82a00c07383ed35ee359ddc2585d
julie-wng <julieweng2011@gmail.com>
2025-12-05 09:54:39 +0100

    fonctionnalite pour se deplacer

--- projet.c
+++ projet.c
@@ -11,6 +11,16 @@ typedef struct{
     int y;
 }Coordonne;
 
+typedef struct infos {
+	char type1;
+	char type2;
+	int nb;
+	int x1;
+	int y1;
+	int x2;
+	int y2;
+}infos;
+
 //affichage du menu
 int menu() {
     int choix;
@@ -26,11 +36,13 @@ void afficherRegles(){
     printf("\nUtilisez les fleches du clavier pour jouer\n");
 }
 
-// Afficher un caractère à une position dans la console avec une couleur et l'ajoute a la matrice 
-void afficherObjet(char plateau[LIGNE][COLONNE],char c, int x, int y, int couleur) {
-    gotoxy(x, y);
-    text_color(couleur);  
+// Afficher un caractère à une position dans la console avec une couleur et l'ajoute a la plateau 
+void afficherObjet(char plateau[LIGNE][COLONNE],char c, int x, int y, int couleur, int couleur_fond) {
+    gotoxy(x*3 + 1, y + 4);
+    text_color(couleur);
+    bg_color(couleur_fond);
     printf("%c",c); 
+    bg_color(0); //réinitialiser le fond à noir 
     plateau[y][x] = c;
 }
 
@@ -58,7 +70,6 @@ char genererItem() {
     return item;
 }
 
-
 //initialise le jeu sans 3 ou plus d'items consecutifs
 void initialisation(char plateau[LIGNE][COLONNE]){
     int item;
@@ -76,7 +87,7 @@ void initialisation(char plateau[LIGNE][COLONNE]){
                 case 'O': couleur = 5;  break; // magenta
                 case 'M': couleur = 1;  break; // bleu
             }
-            afficherObjet(plateau, item, x, y, couleur);
+            afficherObjet(plateau, item, x, y, couleur, 0);
         }
     }
 }
@@ -125,11 +136,7 @@ void initialisation(char plateau[LIGNE][COLONNE]){
 
 // Fonction pour manger un H
 /*void manger_H(char plateau[LIGNE][COLONNE],int *nb_vertical_1,int *nb_vertical_2,int *nb_horizontal){
-    for (int y = 0; y < LIGNE-1; y++) {
-        for (int x = 0; x < COLONNE-1; x++) {
-            plateau[y][x] = ' ';
-        }
-    }
+    
 }*/
 
 int num_item(char item){
@@ -153,6 +160,34 @@ int num_item(char item){
     }
     return val;
 
+}
+
+// Gravitation
+void gravite(char plateau[LIGNE][COLONNE]){
+    for (int x = 0; x < COLONNE; x++) {
+        for (int y = LIGNE - 1; y >= 0; y--) {
+            if (plateau[y][x] == ' ') {
+                for (int k = y - 1; k >= 0; k--) {
+                    if (plateau[k][x] != ' ') {
+                        plateau[y][x] = plateau[k][x];
+                        plateau[k][x] = ' ';
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Fonction pour remplir les trous
+void remplir_trous(char plateau[LIGNE][COLONNE]) {
+    for (int y = 0; y < LIGNE; y++) {
+        for (int x = 0; x < COLONNE; x++) {
+            if (plateau[y][x] == ' ') {
+                plateau[y][x] = genererItem();
+            }
+        }
+    }
 }
 
 //mange le H (vertical = horizontal = 3 items)
@@ -181,125 +216,157 @@ char figure_speciale_H(char plateau[LIGNE][COLONNE],int *score){
     return 0;
 }
 
-//detecte un rectangle
-int rectangle(char plateau[LIGNE][COLONNE], int *largeur,int *longueur,Coordonne debut){
-    for (int y = 0; y < LIGNE-1; y++) {
-        for (int x = 0; x < COLONNE-1; x++) {
+int rectangle(char plateau[LIGNE][COLONNE], int *largeur, int *longueur, Coordonne *debut) {
+    for (int y = 0; y < LIGNE - 1; y++) {
+        for (int x = 0; x < COLONNE - 1; x++) {
             char item = plateau[y][x];
-            int larg = 1;
-            while(x + larg < COLONNE && plateau[y][x+larg] == item){
-                larg++;
-            }
-            int haut = 1;
-            while(y + haut < LIGNE && plateau[y+haut][x] == item){
-                haut++;
-            }
-            if (larg<=haut || haut<=larg){
-                int est_rectangle = 1;
-                for (int i = 0; i < haut && est_rectangle; i++) {
-                    for (int j = 0; j < larg && est_rectangle; j++) {
-                        if (plateau[y + i][x + j] != item) {
+
+            // Tester toutes les largeurs possibles ≥ 2
+            for (int larg = 2; x + larg <= COLONNE; larg++) {
+                // Tester toutes les hauteurs possibles ≥ 2
+                for (int haut = 2; y + haut <= LIGNE; haut++) {
+                    int est_rectangle = 1;
+
+                    // Vérifier bord supérieur et inférieur
+                    for (int i = 0; i < larg; i++) {
+                        if (plateau[y][x + i] != item || plateau[y + haut - 1][x + i] != item) {
                             est_rectangle = 0;
+                            break;
                         }
                     }
-                }
-                
-                if (est_rectangle) {
-                    *largeur = larg;
-                    *longueur = haut;
-                    debut.x = x;
-                    debut.y = y;
-                    return 1; 
+
+                    // Vérifier bord gauche et droit
+                    for (int j = 0; j < haut; j++) {
+                        if (plateau[y + j][x] != item || plateau[y + j][x + larg - 1] != item) {
+                            est_rectangle = 0;
+                            break;
+                        }
+                    }
+
+                    if (est_rectangle) {
+                        *largeur = larg;
+                        *longueur = haut;
+                        debut->x = x;
+                        debut->y = y;
+                        return 1; // Rectangle trouvé
+                    }
                 }
             }
         }
     }
-    return 0;
+    return 0; // Aucun rectangle trouvé
 }
 
 // Fonction pour manger un rectangle
 void manger_rectangle(char plateau[LIGNE][COLONNE], int largeur, int longueur, Coordonne debut, int *score) {
     char item = plateau[debut.y][debut.x];
     
-    //mange le rectangle
-    for (int i = 0; i < longueur; i++) {
-        for (int j = 0; j < largeur; j++) {
-            plateau[debut.y + i][debut.x + j] = ' ';
-        }
+    // Effacer le contour uniquement
+    for (int j = 0; j < largeur; j++) {
+        // ligne du haut
+        plateau[debut.y][debut.x + j] = ' ';
+        // ligne du bas
+        plateau[debut.y + longueur - 1][debut.x + j] = ' ';
     }
-    
+
+    for (int i = 1; i < longueur - 1; i++) {
+        // colonne gauche
+        plateau[debut.y + i][debut.x] = ' ';
+        // colonne droite
+        plateau[debut.y + i][debut.x + largeur - 1] = ' ';
+    }
+
     // Calculer les points : 2 * (largeur x longueur)
     int points = 2 * (largeur * longueur);
     *score += points;
 }
 
-void deplacement_curseur(char touche, int *x, int *y){
-    //pour aller vers le haut
-    if (touche == 'z' && *y > 1){
-        *y = *y - 1;
+//Déplace le curseur et permute si sélection
+void deplacerCurseur(int *y, int *x, int touche, char plateau[LIGNE][COLONNE], int selection, int *ligne_select, int *colonne_select) {
+
+    int nouvY = *y;
+    int nouvX = *x;
+
+    switch(touche) {
+        case 72: if(*y>0) nouvY--; break; // ↑
+        case 80: if(*y<LIGNE-1) nouvY++; break; // ↓
+        case 75: if(*x>0) nouvX--; break; // ←
+        case 77: if(*x<COLONNE-1) nouvX++; break; // → 
     }
-    //pour aller vers le bas
-    if (touche == 's' && *y < LIGNE){
-        *y = *y + 1;
+
+    // Si une lettre est sélectionnée, permuter avec la nouvelle position
+    if(selection) {
+        int diffY = nouvY - *ligne_select;
+        int diffX = nouvX - *colonne_select;
+
+        if((abs(diffY) == 1 && diffX == 0) || (abs(diffX) == 1 && diffY == 0)) {
+            char tmp = plateau[nouvY][nouvX];
+            plateau[nouvY][nouvX] = plateau[*ligne_select][*colonne_select];
+            plateau[*ligne_select][*colonne_select] = tmp;
+
+            // Remettre en majuscule les deux items après permutation
+            plateau[nouvY][nouvX] = toupper(plateau[nouvY][nouvX]);
+            plateau[*ligne_select][*colonne_select] = toupper(plateau[*ligne_select][*colonne_select]);
+        }
     }
-    // pour aller a gauche 
-    if (touche == 'q' && *x > 1){
-        *x = *x - 1;
-    }
-    //pour aller a droite 
-    if (touche == 'd' && *x < COLONNE){
-        *x = *x + 1;
+    // Mettre à jour la position du curseur
+    *y = nouvY;
+    *x = nouvX;
+} 
+
+//Sélectionne/Désélectionne un item
+void selectionne_deselectionne(char plateau[LIGNE][COLONNE], int y, int x, int *selection, int *ligne_select, int *colonne_select) {
+    if(!(*selection)) {
+        // Sélectionner : mettre en minuscule
+        plateau[y][x] = tolower(plateau[y][x]);
+        *selection = 1;
+        *ligne_select = y;
+        *colonne_select = x;
+    } else {
+        plateau[*ligne_select][*colonne_select] = toupper(plateau[*ligne_select][*colonne_select]);
+        *selection = 0;
+        *ligne_select = -1;
+        *colonne_select = -1;
     }
 }
 
-void permuter_items(char plateau[LIGNE][COLONNE], int x1, int y1, int x2, int y2) {
-    char temp = plateau[y1][x1];
-    plateau[y1][x1] = plateau[y2][x2];
-    plateau[y2][x2] = temp;
-}
-
-void manger(){
-
-}
-
-void gravite(char plateau[LIGNE][COLONNE]){
-    for (int x = 0; x < COLONNE; x++) {
-        for (int y = LIGNE - 1; y >= 0; y--) {
-            if (plateau[y][x] == ' ') {
-                for (int k = y - 1; k >= 0; k--) {
-                    if (plateau[k][x] != ' ') {
-                        plateau[y][x] = plateau[k][x];
-                        plateau[k][x] = ' ';
-                        break;
-                    }
-                }
+// Réaffiche toute la grille avec le curseur
+void afficherGrille(char plateau[LIGNE][COLONNE], int curseur_x, int curseur_y) {
+    for(int i = 0; i < LIGNE; i++) {
+        for(int j = 0; j < COLONNE; j++) {
+            char item = plateau[i][j];
+            int couleur;
+            int couleur_fond = 0;
+            
+            // Fond gris si c'est le curseur
+            if(i == curseur_y && j == curseur_x) {
+                couleur_fond = 7;
             }
+            
+            switch(toupper(item)) {
+                case 'S': couleur = 14; break;
+                case 'F': couleur = 4;  break;
+                case 'P': couleur = 2;  break;
+                case 'O': couleur = 5;  break;
+                case 'M': couleur = 1;  break;
+                default: couleur = 7; break;
+            }
+            
+            afficherObjet(plateau, item, j, i, couleur, couleur_fond);
         }
     }
 }
-
-// Fonction pour remplir les trous
-void remplir_trous(char plateau[LIGNE][COLONNE]) {
-    for (int y = 0; y < LIGNE; y++) {
-        for (int x = 0; x < COLONNE; x++) {
-            if (plateau[y][x] == ' ') {
-                plateau[y][x] = genererItem();
-            }
-        }
-    }
-}
-
 
 int jouer_1(char plateau[LIGNE][COLONNE]){
     //contrat
-    gotoxy(20,0);
+    gotoxy(0,0);
     text_color(7);
     printf("Contrat: 20 fraises, 50 oignons et 20 mandarines en 30 coups maximum");
     int nb_fraises = 20;
     int nb_oignon = 50;
     int nb_mandarines = 20;
-
-    int score=0;
+    int nb_coups = 0;
+    int score[5]= {0};
     //variables pour la gestion du temps
     clock_t temps_debut;
     double temps_ecoule;
@@ -308,24 +375,52 @@ int jouer_1(char plateau[LIGNE][COLONNE]){
     temps_ecoule=0;
     temps_debut = clock();
     //variables pour la saise utilisateur (choix de direction de déplacement)
-    char touche='a';
+    int touche;
+    int x = COLONNE / 2;
+    int y = LIGNE / 2;
+    int selection = 0, ligne_select = -1, colonne_select = -1;
     initialisation(plateau);
     do{
         //calcul du temps écoulé
         temps_ecoule = (double)(clock() - temps_debut) / CLOCKS_PER_SEC;
         //affichages
         //affichage du temps écoulé et du score
-        gotoxy(20,2);
+        gotoxy(0,2);
         text_color(7);
         printf("Temps : %.1f s  ", temps_ecoule);
-        gotoxy(20,3);
+        gotoxy(0,3);
         text_color(7);
-        printf("Score : %d  ", score);
+        printf("Score : %d Coups: %d ", score, nb_coups); 
+
+        afficherGrille(plateau, x, y);
+        
+        if(manger)
 
         // partie évènementielle : ce que fait le programme si l'utilisateur appuie sur une touche 
         if(kbhit()){ //si une touche a été appuyée
             touche=getch(); //on récupère la touche
-
+            
+            if(touche == 224) {
+                touche = getch();   // la vraie touche des fleches (72/80/75/77)
+                int ancien_selection = selection;
+                deplacerCurseur(&y, &x, touche, plateau, selection, &ligne_select, &colonne_select);
+                
+                // Si permutation effectuée
+                if(ancien_selection && selection) {
+                    int diffY = y - ligne_select;
+                    int diffX = x - colonne_select;
+                    if((abs(diffY) == 1 && diffX == 0) || (abs(diffX) == 1 && diffY == 0)) {
+                        selection = 0;
+                        ligne_select = -1;
+                        colonne_select = -1;
+                        nb_coups++;
+                    }
+                }
+            }
+            // espace = sélection/désélection
+            else if(touche == 32) {
+                selectionne_deselectionne(plateau, y, x, &selection, &ligne_select, &colonne_select);
+            }
         }
         //Petite pause pour que ça n'aille pas trop vite
         Sleep(50);
